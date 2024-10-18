3ab64ea0dc3c201b25ea88ff5b2d4a9f2571c35e
granPiero <fischer.pj^gmail.com>
2024-10-18 11:40:09 +0000

    2442

--- docs/posts/DOM via Javascript/script2.js
+++ docs/posts/DOM via Javascript/script2.js
@@ -2,7 +2,8 @@ document.getElementById('submitButton2').addEventListener('click', function() {
   // Capture the input value
   var param_string = document.getElementById('inputRef').value;
   var url = "https://jsfapi.netlify.app/.netlify/functions/bgw?param=" + param_string;
-
+  alert(url);
+  
   // set src attribute in iframe element
   document.getElementById('frame2').src = url;
 });
