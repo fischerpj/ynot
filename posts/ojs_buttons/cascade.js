7b28cecc36c85eec37d3032fcb7ded9806b349e4
Pier Fish <fischer.pj@gmail.com>
2026-02-03 18:39:59 +0000

    2606 buttons

--- posts/ojs_buttons/cascade.js
+++
@@ -1,7 +0,0 @@
-const style = document.createElement("style");
-style.textContent = `
-  .ojs-in-a-box-waiting-for-module-import .btn-success {
-    color: white !important;
-  }
-`;
-document.head.appendChild(style);
